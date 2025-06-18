2a884b5d493cabfb4137df416104fb7a4184c3b7
Jhnv07 <jahnavi.yadav007@gmail.com>
2025-06-18 17:51:01 +0530

    leetcode46 updated

---
+++ leetcode46.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int num : nums) {
+            if (current.contains(num)) continue;
+
+            current.add(num);                     // Choose
+            backtrack(nums, current, result);     // Explore
+            current.remove(current.size() - 1);   // Unchoose (backtrack)
+        }
+    }
+}
